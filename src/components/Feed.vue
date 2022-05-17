6c40ce99c0f58481296acd3af2322f8bd0c1dcb8
julia-bak <bakurova.julia159@gmail.com>
2022-05-17 11:47:10 +0300

    login registration feed

---
+++ src/components/Feed.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+
+    <div v-if="error">Something bad happend</div>
+
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link 
+            :to="{
+              name: 'userProfile', 
+              params: {slug: article.author.username}
+            }"
+          >
+            <img :src="article.author.image">
+          </router-link>
+
+          <div class="info">
+            <router-link
+              class="author" 
+              :to="{
+                name: 'userProfile', 
+                params: {slug: article.author.username}
+              }"
+          >
+            {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">
+            ADD TO FAV
+          </div>
+        </div>
+        <router-link :to="{name: 'article', params: {slug: article.slug}}" class="preview-link">
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      <Pagination 
+        :total="feed.articlesCount" 
+        :limit="limit"
+        :current-page="currentPage"
+        :url="baseUrl"
+
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import {actionTypes} from '@/store/modules/feed'
+import Pagination from '@/components/Pagination'
+import {limit} from '@/helpers/vars'
+// import {stringify, parseUrl} from 'query-string'
+
+export default {
+  name: 'AppFeed',
+  data() {
+    return {
+      limit,
+    }
+  },
+  components: {
+    Pagination
+  },
+  props: {
+    apiUrl: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.feed.isLoading,
+      feed: state => state.feed.data,
+      error: state => state.feed.error
+    }),
+    currentPage() {
+      return Number(this.$route.query.page || '1')
+    },
+    baseUrl() {
+      return this.$route.path
+    }
+  },
+  watch: {
+    currentPage(){
+      console.log('cur')
+      this.fetchFeed()
+    }
+  },
+  methods: {
+    fetchFeed(){
+      this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
+    }
+  },
+  mounted() {
+    this.fetchFeed()
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,59 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import GlobalFeed from '@/views/GlobalFeed.vue'
+import Register from '@/views/Register.vue'
+import Login from '@/views/Login.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'globalFeed',
+    component: GlobalFeed
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/feed',
+    name: 'yourFeed',
+    component:  Register
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component:  Register
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component:  Register
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component:  Register
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component:  Register
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component:  Register
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component:  Register
+  },
+  {
+    path: '/profiles/:slug/favourites',
+    name: 'userProfileFavourites',
+    component:  Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component:  Login
+  },
 ]
 
 const router = createRouter({
